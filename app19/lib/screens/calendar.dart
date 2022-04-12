7af29bdfce245f5fc3ba4a0943cba2f9243d27ef
Benito Califano <benitocalifano0@gmail.com>
2022-04-12 15:56:02 +0200

    Added Stateful calendar

--- app19/lib/screens/calendar.dart
+++ app19/lib/screens/calendar.dart
@@ -1,20 +1,115 @@
+import 'package:app19/main.dart';
+import 'package:dynamic_themes/dynamic_themes.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class Calendar extends StatelessWidget {
+class Calendar extends StatefulWidget {
   const Calendar({Key? key}) : super(key: key);
+
+  @override
+  State<Calendar> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  late Map<DateTime, List<Event>> selectedEvents;
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
+  TextEditingController _eventController = TextEditingController();
+
+  @override
+  void initState() {
+    selectedEvents = {};
+    super.initState();
+  }
+
+  List<Event> _getEventsfromDay(DateTime date) {
+    return selectedEvents[date] ?? [];
+  }
+
+  @override
+  void dispose() {
+    _eventController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Calendar')),
       body: _calendar(),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: (() => showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+                  title: Text('New Event'),
+                  content: TextFormField(
+                    controller: _eventController,
+                  ),
+                  actions: [
+                    TextButton(
+                        onPressed: () => Navigator.pop(context),
+                        child: Text('Cancel')),
+                    TextButton(
+                        onPressed: () {
+                          if (_eventController.text.isEmpty) {
+                            Navigator.pop(context);
+                            return;
+                          } else {
+                            if (selectedEvents[_selectedDay] != null) {
+                              selectedEvents[_selectedDay]?.add(
+                                Event(title: _eventController.text),
+                              );
+                            }
+                          }
+                        },
+                        child: Text('Confirm')),
+                  ],
+                ))),
+        label: Text('New Event'),
+        icon: Icon(Icons.add),
+      ),
     );
   }
+
+  Widget _calendar() => TableCalendar(
+        focusedDay: _focusedDay,
+        firstDay: DateTime.utc(2010),
+        lastDay: DateTime.utc(2030),
+        selectedDayPredicate: (day) {
+          return isSameDay(_selectedDay, day);
+        },
+        eventLoader: _getEventsfromDay,
+        headerStyle: HeaderStyle(formatButtonShowsNext: false),
+        calendarStyle: CalendarStyle(
+          todayDecoration:
+              BoxDecoration(color: Colors.blue, shape: BoxShape.circle),
+          selectedDecoration:
+              BoxDecoration(color: Colors.red, shape: BoxShape.circle),
+        ),
+        onDaySelected: (selectedDay, focusedDay) {
+          setState(() {
+            _selectedDay = selectedDay;
+            _focusedDay = focusedDay;
+          });
+        },
+        calendarFormat: _calendarFormat,
+        onFormatChanged: (format) {
+          setState(() {
+            _calendarFormat = format;
+          });
+        },
+        onPageChanged: (focusedDay) {
+          _focusedDay = focusedDay;
+        },
+        startingDayOfWeek: StartingDayOfWeek.monday,
+      );
 }
 
-Widget _calendar() => TableCalendar(
-      focusedDay: DateTime.now(),
-      firstDay: DateTime.utc(2010, 10, 16),
-      lastDay: DateTime.utc(2030, 3, 14),
-    );
+class Event {
+  final String title;
+  Event({required this.title});
+
+  String toString() => this.title;
+}
